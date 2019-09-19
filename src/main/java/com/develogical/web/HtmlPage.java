2c3430d2ee1dadb524e5ba1756888373e71d0be2
Ashley <zcabxli@ucl.ac.uk>
2019-09-19 14:04:59 +0100

    html

--- src/main/java/com/develogical/web/HtmlPage.java
+++ src/main/java/com/develogical/web/HtmlPage.java
@@ -21,6 +21,7 @@ public abstract class HtmlPage {
     }
 
     private void writeFooterTo(PrintWriter writer) {
+        writer.println("<h1>hello</h1>");
         writer.println("</body>");
         writer.println("</html>");
     }
